469b6e406930c764eca0d59aed48fa0be8830abb
Susan Tan <ssusantan9@gmail.com>
2019-04-08 15:33:30 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,3 +1,3 @@
 CREATE TABLE bears (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive BOOLEAN);
 
-INSERT INTO bears (name) VALUES ('Mr. Chocolate')
+INSERT INTO bears (name) VALUES ('Mr. Chocolate', 'Rowdy', 'Tabitha', 'Sergeant Brown', 'Melissa', 'Grinch', 'Wendy', 'unnamed (the bear that killed Tim didn\t have a name; refer back to how to create a record that doesn\t have one value)');
