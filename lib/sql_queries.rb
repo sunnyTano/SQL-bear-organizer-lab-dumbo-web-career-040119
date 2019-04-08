5312f2fc464994f4766c0d652537c1984ffc0276
Susan Tan <ssusantan9@gmail.com>
2019-04-08 15:53:24 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,8 +18,12 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive = 1, ORDERY BY name, age ASC"
+  "SELECT name, age FROM bears WHERE alive = 1, ORDERY BY ASC"
 end
+
+# SELECT CompanyName, ContactName, City, Country
+#   FROM Supplier
+# ORDER BY CompanyName
 
 def selects_oldest_bear_and_returns_name_and_age
   "Write your SQL query here"
