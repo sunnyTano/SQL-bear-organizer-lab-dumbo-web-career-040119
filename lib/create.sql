7b685ccc4c7ff4d8c996a083bc9327dce2339855
Susan Tan <ssusantan9@gmail.com>
2019-04-08 15:32:20 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,3 +1,3 @@
 CREATE TABLE bears (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive BOOLEAN);
 
-INSERT INTO 
+INSERT INTO bears (name) VALUES ('Mr. Chocolate', 'Rowdy', 'Tabitha', 'Sergeant Brown', 'Melissa', 'Grinch', 'Wendy', '')
