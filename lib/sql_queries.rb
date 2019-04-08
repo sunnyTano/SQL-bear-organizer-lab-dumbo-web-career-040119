67172e35cb4aa3659e82c6a387990d2f4b48955d
Susan Tan <ssusantan9@gmail.com>
2019-04-08 15:48:05 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,7 @@
 
 
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age FROM bears WHERE gender = 'F'"
+  "SELECT name, age FROM bears WHERE gender LIKE 'f'"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
